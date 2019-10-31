95cf626b440226e99fdbd81bde57645d38b68889
xc7852144840 <xc7852144@gmail.com>
2019-11-01 00:21:16 +0800

    123

---
+++ assets/Js/Picture.js
@@ -0,0 +1,9 @@
+function showscr1(){scr1.style.display="block"}
+			function showscr2(){scr2.style.display="block"}
+			function showscr3(){scr3.style.display="block"}
+			function showscr4(){scr4.style.display="block"}
+
+			function closescr1(){scr1.style.display="none"}
+			function closescr2(){scr2.style.display="none"}
+			function closescr3(){scr3.style.display="none"}
+	        function closescr4(){scr4.style.display="none"}
---
+++ assets/Js/index.js
@@ -0,0 +1,160 @@
+ function showscr1(){
+	        scr1.style.display="block"
+	        document.getElementById('btn4').disabled = true;
+	        document.getElementById('btn2').disabled = true;
+	        document.getElementById('btn3').disabled = true;
+	        document.getElementById('btn5').disabled = true;
+	        document.getElementById('btn6').disabled = true;
+	        document.getElementById('btn7').disabled = true;
+	        document.getElementById('btn8').disabled = true;
+	        }
+	        function closescr1(){
+	        scr1.style.display="none"
+	        document.getElementById('btn4').disabled = false;
+	        document.getElementById('btn2').disabled = false;
+	        document.getElementById('btn3').disabled = false;
+	        document.getElementById('btn5').disabled = false;
+	        document.getElementById('btn6').disabled = false;
+	        document.getElementById('btn7').disabled = false;
+	        document.getElementById('btn8').disabled = false;
+	        }
+	        function showscr2(){
+	        scr2.style.display="block"
+	        document.getElementById('btn1').disabled = true;
+	        document.getElementById('btn4').disabled = true;
+	        document.getElementById('btn3').disabled = true;
+	        document.getElementById('btn5').disabled = true;
+	        document.getElementById('btn6').disabled = true;
+	        document.getElementById('btn7').disabled = true;
+	        document.getElementById('btn8').disabled = true;
+	        }
+	        function closescr2(){
+	        scr2.style.display="none"
+	        document.getElementById('btn1').disabled = false;
+	        document.getElementById('btn4').disabled = false;
+	        document.getElementById('btn3').disabled = false;
+	        document.getElementById('btn5').disabled = false;
+	        document.getElementById('btn6').disabled = false;
+	        document.getElementById('btn7').disabled = false;
+	        document.getElementById('btn8').disabled = false;
+	        }
+	        function showscr3(){
+	        scr3.style.display="block"
+	        document.getElementById('btn1').disabled = true;
+	        document.getElementById('btn2').disabled = true;
+	        document.getElementById('btn4').disabled = true;
+	        document.getElementById('btn5').disabled = true;
+	        document.getElementById('btn6').disabled = true;
+	        document.getElementById('btn7').disabled = true;
+	        document.getElementById('btn8').disabled = true;
+	        }
+	        function closescr3(){
+	        scr3.style.display="none"
+	        document.getElementById('btn1').disabled = false;
+	        document.getElementById('btn2').disabled = false;
+	        document.getElementById('btn4').disabled = false;
+	        document.getElementById('btn5').disabled = false;
+	        document.getElementById('btn6').disabled = false;
+	        document.getElementById('btn7').disabled = false;
+	        document.getElementById('btn8').disabled = false;
+	        }
+	        function showscr4(){
+	        scr4.style.display="block"
+	        document.getElementById('btn1').disabled = true;
+	        document.getElementById('btn2').disabled = true;
+	        document.getElementById('btn3').disabled = true;
+	        document.getElementById('btn5').disabled = true;
+	        document.getElementById('btn6').disabled = true;
+	        document.getElementById('btn7').disabled = true;
+	        document.getElementById('btn8').disabled = true;
+	        }
+	        function closescr4(){
+	        scr4.style.display="none"
+	        document.getElementById('btn1').disabled = false;
+	        document.getElementById('btn2').disabled = false;
+	        document.getElementById('btn3').disabled = false;
+	        document.getElementById('btn5').disabled = false;
+	        document.getElementById('btn6').disabled = false;
+	        document.getElementById('btn7').disabled = false;
+	        document.getElementById('btn8').disabled = false;
+	        }
+	        function showscr5(){
+	        scr5.style.display="block"
+	        document.getElementById('btn1').disabled = true;
+	        document.getElementById('btn2').disabled = true;
+	        document.getElementById('btn3').disabled = true;
+	        document.getElementById('btn4').disabled = true;
+	        document.getElementById('btn6').disabled = true;
+	        document.getElementById('btn7').disabled = true;
+	        document.getElementById('btn8').disabled = true;
+	        }
+	        function closescr5(){
+	        scr5.style.display="none"
+	        document.getElementById('btn1').disabled = false;
+	        document.getElementById('btn2').disabled = false;
+	        document.getElementById('btn3').disabled = false;
+	        document.getElementById('btn4').disabled = false;
+	        document.getElementById('btn6').disabled = false;
+	        document.getElementById('btn7').disabled = false;
+	        document.getElementById('btn8').disabled = false;
+	        }
+	        function showscr6(){
+	        scr6.style.display="block"
+	        document.getElementById('btn1').disabled = true;
+	        document.getElementById('btn2').disabled = true;
+	        document.getElementById('btn3').disabled = true;
+	        document.getElementById('btn5').disabled = true;
+	        document.getElementById('btn4').disabled = true;
+	        document.getElementById('btn7').disabled = true;
+	        document.getElementById('btn8').disabled = true;
+	        }
+	        function closescr6(){
+	        scr6.style.display="none"
+	        document.getElementById('btn1').disabled = false;
+	        document.getElementById('btn2').disabled = false;
+	        document.getElementById('btn3').disabled = false;
+	        document.getElementById('btn5').disabled = false;
+	        document.getElementById('btn4').disabled = false;
+	        document.getElementById('btn7').disabled = false;
+	        document.getElementById('btn8').disabled = false;
+	        }
+	        function showscr7(){
+	        scr7.style.display="block"
+	        document.getElementById('btn1').disabled = true;
+	        document.getElementById('btn2').disabled = true;
+	        document.getElementById('btn3').disabled = true;
+	        document.getElementById('btn5').disabled = true;
+	        document.getElementById('btn6').disabled = true;
+	        document.getElementById('btn4').disabled = true;
+	        document.getElementById('btn8').disabled = true;
+	        }
+	        function closescr7(){
+	        scr7.style.display="none"
+	        document.getElementById('btn1').disabled = false;
+	        document.getElementById('btn2').disabled = false;
+	        document.getElementById('btn3').disabled = false;
+	        document.getElementById('btn5').disabled = false;
+	        document.getElementById('btn6').disabled = false;
+	        document.getElementById('btn4').disabled = false;
+	        document.getElementById('btn8').disabled = false;
+	        }
+	        function showscr8(){
+	        scr8.style.display="block"
+	        document.getElementById('btn1').disabled = true;
+	        document.getElementById('btn2').disabled = true;
+	        document.getElementById('btn3').disabled = true;
+	        document.getElementById('btn5').disabled = true;
+	        document.getElementById('btn6').disabled = true;
+	        document.getElementById('btn7').disabled = true;
+	        document.getElementById('btn4').disabled = true;
+	        }
+	        function closescr8(){
+	        scr8.style.display="none"
+	        document.getElementById('btn1').disabled = false;
+	        document.getElementById('btn2').disabled = false;
+	        document.getElementById('btn3').disabled = false;
+	        document.getElementById('btn5').disabled = false;
+	        document.getElementById('btn6').disabled = false;
+	        document.getElementById('btn7').disabled = false;
+	        document.getElementById('btn4').disabled = false;
+	        }
